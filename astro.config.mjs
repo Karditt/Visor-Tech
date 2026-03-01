707a28c683e114c078cf368baf54d332de895598
Kardit <mandrov.z.k@gmail.com>
2026-03-01 18:14:55 +0300

    config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import react from '@astrojs/react';
 import node from '@astrojs/node';
@@ -8,11 +9,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://vizortex.ru',
   output: 'server',
   adapter: node({
     mode: 'standalone',
   }),
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
